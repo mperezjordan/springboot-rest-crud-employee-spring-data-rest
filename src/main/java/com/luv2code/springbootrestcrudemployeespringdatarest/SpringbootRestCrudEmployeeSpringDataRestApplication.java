737d6c26c3c0753b35fc42ba2557d6c82dc6d517
Mario Perez-Jordan <mpjmario@gmail.com>
2023-08-15 11:45:44 -0400

    Initial commit

---
+++ src/main/java/com/luv2code/springbootrestcrudemployeespringdatarest/SpringbootRestCrudEmployeeSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootrestcrudemployeespringdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCrudEmployeeSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestCrudEmployeeSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/luv2code/springbootrestcrudemployeespringdatarest/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.luv2code.springbootrestcrudemployeespringdatarest.dao;
+
+import com.luv2code.springbootrestcrudemployeespringdatarest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
